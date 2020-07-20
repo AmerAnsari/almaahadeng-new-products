e4ada9ba9f2c4a520b37006bdff7fef7be6c1900
Amir Savand <amir@savandbros.com>
2020-07-20 16:52:21 +0300

    Update to version 2.0

---
+++ src/asset/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+  console.log("Almaahad Engineering, Special Attachments project by SavandBros.");
+  console.log("https://www.savandbros.com");
+  window.BSN.initCallback();
+});
